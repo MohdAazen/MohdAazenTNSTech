fd05803453f25603ba91857188d56f01a324860f
MohdAazen <sayyedaazen81@gmail.com>
2024-06-21 11:51:11 +0530

    commiting five

---
+++ MohdAazenSayyed/src/com/company/programsAssignmentDay2/Student.java
@@ -0,0 +1,20 @@
+/*Day2 Assignment
+Create a default constructor in the class Student that prints a message “Student object is created” when the program is run.
+ */
+
+package com.company.programsAssignmentDay2;
+
+public class Student {
+
+	public Student()
+	{
+		System.out.println("Student object is created");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student stud  = new Student();
+		
+	}
+
+}
